4952dac06fb258d96454b154e7209de5b80f0251
CharlieCarpenter <charles.carpenter@cuanschutz.edu>
2022-04-11 10:11:26 -0600

    tinkering

--- TreeSeg/src/segTree_CC.cpp
+++ TreeSeg/src/segTree_CC.cpp
@@ -46,7 +46,7 @@ List segTree_CC(arma::mat K, arma::vec R,
   // generate multiscale bounds from stepR package
   List bou = boundsCall_CC(R, lengths, alpha, q, fam);
   
-  Rcpp::print(bou);
+  Rcpp::print(bou[0:4]);
   
   NumericVector lower = bou["lower"];                  
   NumericVector upper = bou["upper"];
